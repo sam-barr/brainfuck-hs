8ef3aa2a77a6a12dd8ab20a1b53e0424f692504c
Sam Barr <sbarr@oberlin.edu>
2018-03-16 21:57:57 -0400

    Added Comments

--- bf.hs
+++ bf.hs
@@ -5,6 +5,8 @@ import Data.Bifunctor
 import System.Environment
 import Tape
 
+-- Data type for brainfuck character
+-- treats expressions in between brackets as a single character
 data BFChar = INC
             | DEC
             | ADD
@@ -14,22 +16,11 @@ data BFChar = INC
             | BRACKET BFExp
             deriving (Show)
 
+-- a brainfuck expression is just a list of brainfuck characters
 type BFExp = [BFChar]
 
-action :: BFChar -> Tape Int -> IO (Tape Int)
-action INC tape       = return $ moveR tape
-action DEC tape       = return $ moveL tape
-action ADD (T l v r)  = return $ T l (v+1) r
-action SUB (T l v r)  = return $ T l (v-1) r
-action PRINT tape     = putChar (chr (val tape)) >> return tape
-action READ (T l _ r) = getChar >>= \x -> return (T l (ord x) r)
-action (BRACKET exp) tape =
-  if (val tape) == 0 then
-    return tape
-  else do
-    res <- eval (exp, tape)
-    action (BRACKET exp) res
-
+-- code parser
+-- need to figure '[' with unmatched ']'
 parse :: String -> (BFExp, String)
 parse []     = ([],[])
 parse (c:cs) = 
@@ -47,6 +38,7 @@ parse (c:cs) =
 
     app x = first (x:) rec
 
+-- runs parser
 runParse :: String -> BFExp
 runParse str
   | null rest = exp
@@ -54,11 +46,29 @@ runParse str
   where
     (exp, rest) = parse str
 
+-- Initial tape, set to all zeroes
 zeroTape :: Tape Int
 zeroTape = T (repeat 0) 0 (repeat 0)
 
+-- the state of the program
 type State = (BFExp, Tape Int)
 
+-- maps a brainfuck character to an action
+action :: BFChar -> Tape Int -> IO (Tape Int)
+action INC tape       = return $ moveR tape
+action DEC tape       = return $ moveL tape
+action ADD (T l v r)  = return $ T l (v+1) r
+action SUB (T l v r)  = return $ T l (v-1) r
+action PRINT tape     = putChar (chr (val tape)) >> return tape
+action READ (T l _ r) = getChar >>= \x -> return (T l (ord x) r)
+action (BRACKET exp) tape =
+  if (val tape) == 0 then
+    return tape
+  else do
+    res <- eval (exp, tape)
+    action (BRACKET exp) res
+
+-- evaluates the code
 eval :: State -> IO (Tape Int)
 eval ([],x) = return x
 eval (b:bs, tape) = action b tape >>= eval . (bs, )
--- tape.hs
+++ tape.hs
@@ -5,6 +5,7 @@ import Control.Comonad
 import Data.Monoid
 import Data.List (intersperse)
 
+-- Tape datatype
 data Tape a = T [a] a [a] deriving (Eq)
 
 instance Show a => Show (Tape a) where
@@ -13,20 +14,26 @@ instance Show a => Show (Tape a) where
       lstring = listShow $ reverse $ take 10 l
       rstring = listShow $ take 10 r
 
+-- used to print the tape
 listShow :: Show a => [a] -> String
 listShow [] = ""
 listShow x  = intersperse ' ' $ (x >>= show)
 
+-- value of head of tape
 val :: Tape a -> a
 val (T _ v _) = v
 
+-- moves the tape right
 moveR :: Tape a -> Tape a
 moveR (T l v (r:rs)) = T (v:l) r rs
 moveR _ = error "No more tape"
 
+-- moves the tape left
 moveL :: Tape a -> Tape a
 moveL (T (l:ls) v r) = T ls l (v:r)
 moveL _ = error "No more tape"
+
+-- Tape instances that I don't use
 
 instance Functor Tape where
   fmap f (T l v r) = T (map f l) (f v) (map f r)
